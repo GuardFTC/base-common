5ff2c374f42a3105ec4106a74eb78f0f7a4843a7
fengtiecheng <17615007230@163.com>
2023-06-30 17:48:07 +0800

    feat(result): 简易分页类调整
    1.简易分页类调整

--- src/main/java/com/ftc/basecommon/result/SimplePageInfo.java
+++ src/main/java/com/ftc/basecommon/result/SimplePageInfo.java
@@ -1,6 +1,7 @@
 package com.ftc.basecommon.result;
 
 import cn.hutool.core.collection.CollUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.github.pagehelper.PageInfo;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -8,7 +9,7 @@ import lombok.Data;
 import java.util.List;
 
 /**
- * @author: 冯铁城 [17615007230@163.com]
+ * @author: 冯铁城
  * @date: 2022-12-15 20:51:43
  * @describe: 简单分页对象
  */
@@ -16,16 +17,42 @@ import java.util.List;
 public class SimplePageInfo<T> {
 
     @ApiModelProperty("页数")
-    private Integer pageNum;
+    private long pageNum;
 
     @ApiModelProperty("页码")
-    private Integer pageSize;
+    private long pageSize;
 
     @ApiModelProperty("总数")
-    private Long total;
+    private long total;
 
     @ApiModelProperty("分页数据")
     private List<T> data;
+
+    /**
+     * 初始化简单分页实体类,用于数据库查询分页,响应数据为非PO数据
+     *
+     * @param pageInfo pageInfo实体类
+     * @param data     响应数据集合
+     * @param <V>      pageInfo对象类型
+     * @param <T>      响应数据对象类型
+     * @return 简单分页实体类
+     */
+    public static <V, T> SimplePageInfo<T> init(PageInfo<V> pageInfo, List<T> data) {
+
+        //1.创建对象
+        SimplePageInfo<T> simplePageInfo = new SimplePageInfo<>();
+
+        //2.设置基础参数
+        simplePageInfo.setPageNum(pageInfo.getPageNum());
+        simplePageInfo.setPageSize(pageInfo.getPageSize());
+        simplePageInfo.setTotal(pageInfo.getTotal());
+
+        //3.设置data
+        simplePageInfo.setData(data);
+
+        //4.返回
+        return simplePageInfo;
+    }
 
     /**
      * 初始化简单分页实体类,用于数据库查询分页
@@ -46,6 +73,56 @@ public class SimplePageInfo<T> {
 
         //3.设置data
         simplePageInfo.setData(pageInfo.getList());
+
+        //4.返回
+        return simplePageInfo;
+    }
+
+    /**
+     * 初始化简单分页实体类,用于数据库查询分页,响应数据为非PO数据
+     *
+     * @param pageInfo pageInfo实体类
+     * @param data     响应数据集合
+     * @param <V>      pageInfo对象类型
+     * @param <T>      响应数据对象类型
+     * @return 简单分页实体类
+     */
+    public static <V, T> SimplePageInfo<T> init(Page<V> pageInfo, List<T> data) {
+
+        //1.创建对象
+        SimplePageInfo<T> simplePageInfo = new SimplePageInfo<>();
+
+        //2.设置基础参数
+        simplePageInfo.setPageNum(pageInfo.getCurrent());
+        simplePageInfo.setPageSize(pageInfo.getSize());
+        simplePageInfo.setTotal(pageInfo.getTotal());
+
+        //3.设置data
+        simplePageInfo.setData(data);
+
+        //4.返回
+        return simplePageInfo;
+    }
+
+    /**
+     * 初始化简单分页实体类,用于数据库查询分页
+     *
+     * @param pageInfo pageInfo实体类
+     * @param <T>      分页数据泛型
+     * @return 简单分页实体类
+     */
+    public static <T> SimplePageInfo<T> init(Page<T> pageInfo) {
+
+        //1.创建对象
+        SimplePageInfo<T> simplePageInfo = new SimplePageInfo<>();
+
+        //2.设置基础参数
+        simplePageInfo.setPageNum(pageInfo.getCurrent());
+        simplePageInfo.setPageSize(pageInfo.getSize());
+        simplePageInfo.setTotal(pageInfo.getTotal());
+
+        //3.设置data
+        simplePageInfo.setData(pageInfo.getRecords());
 
         //4.返回
         return simplePageInfo;
